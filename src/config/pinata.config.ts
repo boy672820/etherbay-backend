18b3be33c859ff1b63d583b798a0db3078e04623
이선주 <seondev@iseonjuui-MacBookPro.local>
2022-03-10 01:44:15 +0900

    production ip

--- src/config/pinata.config.ts
+++ src/config/pinata.config.ts
@@ -2,7 +2,6 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('pinata', () => ({
   apiUrl: process.env.PINATA_API_URL,
-  gatewayUrl: process.env.PINATA_GATEWAY_URL,
   key: process.env.PINATA_KEY,
   secret: process.env.PINATA_SECRET,
   jwt: process.env.PINATA_JWT,
