486fefeb368fcdb1f2c365a3c304e6529c2c88b7
seonzoo <boy672820@gmail.com>
2022-03-15 23:19:18 +0900

    set docker

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: 'http://10.178.0.2:3000', credentials: true });
+  app.enableCors({ origin: '*', credentials: true });
 
   await app.listen(process.env.PORT || 3000);
 }
